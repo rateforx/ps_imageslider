3db637d60f833733dd547a760f3f3af4473e4a57
Progi1984 <franck.lefevre@prestashop.com>
2020-05-29 09:05:21 +0200

    Fixed include

--- ps_imageslider.php
+++ ps_imageslider.php
@@ -39,7 +39,7 @@ if (file_exists($autoloadPath)) {
     require_once $autoloadPath;
 }
 
-include_once(_PS_MODULE_DIR_.'ps_imageslider/Ps_HomeSlide.php');
+include_once(__DIR__ . '/Ps_HomeSlide.php');
 
 class Ps_ImageSlider extends Module implements WidgetInterface
 {
